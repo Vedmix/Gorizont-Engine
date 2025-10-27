81a0c01c38907e8b4a5719d78d9ddcae849ca8e8
Daunito228 <rep.gep1945@gmail.com>
2025-10-27 05:12:00 +0300

    started Map class

--- proj/headers/Map.hpp
+++ proj/headers/Map.hpp
@@ -1,1 +1,13 @@
 #pragma once
+#include <memory>
+#include <set>
+#include "../headers/Object2D.hpp"
+
+class Map
+{
+private:
+    std::set<std::shared_ptr<Object2D>> objectSet;
+public:
+    Map();
+    ~Map();
+};

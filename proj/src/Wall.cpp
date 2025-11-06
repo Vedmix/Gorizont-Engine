0b1df2482225ee88758838f3011fc5c47508fa24
Daunito228 <rep.gep1945@gmail.com>
2025-11-06 23:21:31 +0300

    fixed wall

--- proj/src/Wall.cpp
+++ proj/src/Wall.cpp
@@ -5,7 +5,11 @@ Wall::Wall(const Point2D& _position, const int _width, const int _lenght, unsign
     double lenght = _lenght / 2;
     double x = _position.getX();
     double y = _position.getY();
-    points ={Point2D(x + lenght, y + width), Point2D(x - lenght, y + width), Point2D(x + lenght, y - width), Point2D(x - lenght, y - width)};
+    points.resize(4);
+    points[0]=(Point2D(x + lenght, y + width));
+    points[1]=(Point2D(x + lenght, y - width));
+    points[2]=(Point2D(x - lenght, y - width));
+    points[3]=(Point2D(x - lenght, y + width));
 }
 
 Wall::Wall(const Wall& other):Polygon2D(other){

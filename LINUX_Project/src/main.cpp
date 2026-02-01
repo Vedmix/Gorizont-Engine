e7929395ebe31dd542caf5b028020b8e9378d37c
Vedmix <vedmix.work@gmail.com>
2026-02-02 01:52:27 +0300

    Added first work SFML/QT Makefile

--- LINUX_Project/src/main.cpp
+++ LINUX_Project/src/main.cpp
@@ -1,9 +1,20 @@
+#include "../headers/MainMenuWindow.hpp"
 #include "../headers/settings.hpp"
 #include "../headers/World.hpp"
 
-int main()
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char *argv[])
 {
-    World world;
-    world.run();
-    return 0;
+    if(USE_QT){
+        QApplication app(argc, argv);
+        MainMenuWindow mainWindow;
+        mainWindow.show();
+        return app.exec();
+    } else{
+        World world;
+        world.run();
+        return 0;
+    }
 }

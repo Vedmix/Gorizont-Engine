776c87cbf16f773db279504cda44ba3621e3f504
Vedmix <vedmix.work@gmail.com>
2026-02-06 23:06:26 +0300

    Added simple settings interfacce

--- LINUX_Project/headers/SettingsWindow.hpp
+++ LINUX_Project/headers/SettingsWindow.hpp
@@ -6,6 +6,9 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QApplication>
+#include <QLabel>
+#include <QSlider>
+#include <QRadioButton>
 
 class SettingsWindow : public QWidget
 {
@@ -18,8 +21,36 @@ signals:
     void backToMenu();
 
 protected:
+
+    void initSliders();
+    void initRadioButtons();
+    void initButtons();
+
     void keyPressEvent(QKeyEvent *event) override;
 
 private slots:
     void onBackButtonClicked();
+
+private:
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    QVBoxLayout *slidersLayout = new QVBoxLayout();
+    QHBoxLayout *radioButtonsLayout = new QHBoxLayout();
+    QHBoxLayout *buttonsLayout = new QHBoxLayout();
+
+    const std::vector<QString> sliderNames = {
+        "FOV",
+        "Graphics",
+        "Distance",
+        "Speed"
+    };
+
+    const std::vector<QString> radioButtonNames = {
+        "DRUGS MOD"
+    };
+
+    const std::vector<QString> buttonNames = {
+        "Сохранить",
+        "По умолчанию",
+        "Выход"
+    };
 };
--- LINUX_Project/src/ui/SettingsWindow.cpp
+++ LINUX_Project/src/ui/SettingsWindow.cpp
@@ -2,34 +2,78 @@
 
 SettingsWindow::SettingsWindow(QWidget *parent): QWidget(parent)
 {
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    QHBoxLayout *buttonLayout = new QHBoxLayout();
+    initSliders();
+    initRadioButtons();
+    initButtons();
 
-    buttonLayout->addStretch();
-
-    QPushButton *backButton = new QPushButton("Выход в меню", this);
-    backButton->setFixedSize(200, 50);
-    backButton->setStyleSheet(
-        "QPushButton {"
-        "    background-color: #4CAF50;"
-        "    border: none;"
-        "    color: white;"
-        "    font-size: 14px;"
-        "    border-radius: 5px;"
-        "}"
-        "QPushButton:hover {"
-        "    background-color: #45a049;"
-        "}"
-        );
-
-    connect(backButton, &QPushButton::clicked, this, &SettingsWindow::onBackButtonClicked);
-
-    buttonLayout->addWidget(backButton);
-    buttonLayout->addStretch();
-
-    mainLayout->addLayout(buttonLayout);
+    mainLayout->addLayout(slidersLayout);
+    mainLayout->addLayout(radioButtonsLayout);
+    mainLayout->addLayout(buttonsLayout);
+    mainLayout->addStretch();
 
     setLayout(mainLayout);
+}
+
+void SettingsWindow::initSliders(){
+
+    slidersLayout->setAlignment(Qt::AlignCenter);
+    slidersLayout->addStretch();
+
+    for(size_t i = 0; i < sliderNames.size(); i++){
+        QHBoxLayout *sliderLayout = new QHBoxLayout();
+
+        QLabel *sliderName = new QLabel(sliderNames[i], this);
+        sliderName->setFixedWidth(70);
+
+        QLabel *sliderValue= new QLabel(QString::number(i), this);
+        sliderValue->setFixedWidth(20);
+
+        QSlider *slider = new QSlider(Qt::Horizontal, this);
+        slider->setMaximumWidth(300);
+
+        sliderLayout->addWidget(sliderName);
+        sliderLayout->addWidget(sliderValue);
+        sliderLayout->addWidget(slider);
+
+        slidersLayout->addLayout(sliderLayout);
+    }
+}
+
+void SettingsWindow::initRadioButtons(){
+
+    for(size_t i = 0; i < radioButtonNames.size(); i++){
+        QRadioButton *radioButton = new QRadioButton(radioButtonNames[i], this);
+        radioButton->setFixedSize(200, 50);
+
+        radioButtonsLayout->addWidget(radioButton);
+    }
+}
+
+void SettingsWindow::initButtons(){
+
+    buttonsLayout->setAlignment(Qt::AlignCenter);
+
+    for(size_t i = 0; i < buttonNames.size(); i++){
+        QPushButton *button = new QPushButton(buttonNames[i], this);
+        button->setFixedSize(200, 50);
+        button->setStyleSheet(
+            "QPushButton {"
+            "    background-color: #4CAF50;"
+            "    border: none;"
+            "    color: white;"
+            "    font-size: 14px;"
+            "    border-radius: 5px;"
+            "}"
+            "QPushButton:hover {"
+            "    background-color: #45a049;"
+            "}"
+            );
+        buttonsLayout->addWidget(button);
+
+        if (i == 2){
+            connect(button, &QPushButton::clicked, this, &SettingsWindow::onBackButtonClicked);
+        }
+    }
 }
 
 void SettingsWindow::keyPressEvent(QKeyEvent *event)
